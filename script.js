2f962bdb1ab665b0af3690004651b2808bbda8d4
Laci Beres <3532853+laciberes@users.noreply.github.com>
2023-10-30 20:41:52 +0100

    Updated version with all functions working Added a function for user choice and also a new game() function to run the game rounds 5 times

--- script.js
+++ script.js
@@ -1,57 +1,107 @@
-const rps = ["rock", "paper", "scissors"];
+    const rps = ["rock", "paper", "scissors"];
 
             // The random choice of the computer
-            
             function getComputerChoice() {
-
                 return rps[Math.floor(Math.random()*3)];
             }
 
-            let computerSelection = getComputerChoice();
-
-
-            // The choice of the player entered via prompt
-
-            let playerSelection = (prompt('Rock, paper or scissors?')).toLowerCase();
+            // Player's choice from prompt
+            function getUserChoice() {
+            return prompt('Rock, paper or scissors?').toLowerCase();
+            }
+            
+            // Variables to store score of a game
+            let playerScore = 0;
+            let computerScore = 0;
+            // Variables to keep count of total score
+            let playerScoreCount;
+            let computerScoreCount;
 
             
-            // This line confirms both selections in the console log
-            console.log(playerSelection + " vs " + computerSelection)
-
-           
+            // -- FUNCTION THAT PLAYS THE INDIVIDUAL ROUNDS -- //
             function playRound() {
+
+                let playerSelection = getUserChoice();    
+                let computerSelection = getComputerChoice();
+            
+
+            // console.log("Computer says: " + computerSelection + " --- " + "You say:  " + playerSelection);
+
                 
                 // User choice: ROCK
                 if (playerSelection === "rock" && computerSelection === "rock") {
-                    console.log("No winner, play again!");
+                    playerScoreCount = ++playerScore;
+                    computerScoreCount = ++computerScore;
+                    return "No winner, play again!";
+                    // console.log("Player: " + playerScore + "  " + "Computer:" + computerScore);
 
                 } else if (playerSelection === "rock" && computerSelection === "paper") {
-                    console.log("Computer won! Paper beats rock."); 
+                    // playerScoreCount = playerScore;
+                    computerScoreCount = ++computerScore;
+                    return "Computer won! Paper beats rock.";
+                    // console.log ("Player: " + playerScore + "  " + "Computer:" + computerScore);
                 } 
 
                 // User choice: PAPER
                 else if (playerSelection === "paper" && computerSelection === "paper") {
-                    console.log("No winner, play again!");
+                    playerScoreCount = ++playerScore;
+                    computerScoreCount = ++computerScore;
+                    return "No winner, play again!";
+                    // console.log("Player: " + playerScore + "  " + "Computer:" + computerScore);
 
                 } else if (playerSelection === "paper" && computerSelection === "scissors") {
-                    console.log("Computer won! Scissors beat paper."); 
+                    // playerScoreCount = playerScore;
+                    computerScoreCount = ++computerScore;
+                    return "Computer won! Scissors beat paper.";
+                    // console.log("Player: " + playerScore + "  " + "Computer:" + computerScore);
                 } 
 
                 // User choice: SCISSORS
                 else if (playerSelection === "scissors" && computerSelection === "scissors") {
-                    console.log("No winner, play again!");
+                    playerScoreCount = ++playerScore;
+                    computerScoreCount = ++computerScore;
+                    return "No winner, play again!";
+                    // console.log("Player: " + playerScore + "  " + "Computer:" + computerScore);
                 } 
                 else if (playerSelection === "scissors" && computerSelection === "rock") {
-                    console.log("Computer won! Rock beat scissors"); 
+                    // playerScoreCount = playerScore;
+                    computerScoreCount = ++computerScore;
+                    return "Computer won! Rock beat scissors.";
+                    // console.log("Player: " + playerScore + "  " + "Computer:" + computerScore);
                 } 
                 
                 // User choice: any other words
                 else if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-                console.log( "Wrong input, please write: rock or paper or scissors.");
+                    return "Wrong input, please write: rock or paper or scissors.";
                 } 
                 
-                else { console.log("You won!")}
+                else { 
+                playerScoreCount = ++playerScore;
+                computerScoreCount = computerScore;
+                return "You won!"
+                // console.log("Player: " + playerScore + "  " + "Computer:" + computerScore);
+                }
 
             }
+            
+            // Function to run 5 rounds, display the round's and the the final score
+            function game() {
 
-            playRound(playerSelection, computerSelection);
+            for (let i = 1; i < 6; i++) {
+                console.log(playRound());
+            }
+
+                if (playerScoreCount > computerScoreCount) {
+                    console.log("End of 5 rounds. Player won by " + playerScoreCount + ":" + computerScoreCount);
+                } else if (playerScoreCount < computerScoreCount) {
+                    console.log("End of 5 rounds. Computer won by " + playerScoreCount + ":" + computerScoreCount);
+                } else { console.log("End of 5 rounds. Its a draw! " + playerScoreCount + " : " + computerScoreCount);
+                
+                console.log(playerScoreCount + "  " + computerScoreCount);
+            }
+           }
+
+           game();
+        
+           
+            
